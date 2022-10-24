8fa5f40a1e3aacf55ff7c289de6372ef72a9fa84
alatos2 <alabitosin60@gmail.com>
2022-10-24 10:57:00 +0000

    - created the Artiste model - created the Song model - created the Lyric Model - run migrations

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from musicapp.models import Artiste, Song, Lyric
 
 # Register your models here.
+@admin.register(Artiste)
+class ArtisteAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'age']
+
+admin.site.register(Song)
+
+admin.site.register(Lyric)
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    date_released = models.DateField()
+    likes = models.TextField()
+    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+class Lyric(models.Model):
+    content = models.TextField()
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
